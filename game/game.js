7030e9f77ab434e7e3c5963f7c66741f6c67e867
Jalen Battle <jalenbattle78@gmail.com>
2024-12-09 14:56:36 -0500

    now the game works

--- game/game.js
+++ game/game.js
@@ -12,7 +12,7 @@ class Attr {
         this.name = name;
         this.value = value;
     }
-}
+};
 
 class Fighter {
     constructor(name) {
@@ -31,18 +31,54 @@ class Fighter {
         Dex: ${this.attrList[1].value}
         `;
     }
+    attack(who) {
+        const myStr = this.attrList[0].value;
+        const myDex = this.attrList[1].value;
+        const theirDex = who.attrList[1].value;
+        const chanceOfSuccess = (50 + (myDex - theirDex)) / 100;
+        if (Math.random() >= chanceOfSuccess) {
+            const damage =  Math.ceil(myStr * Math.random());
+            who.health -= damage;
+            return "The " + this.name + " hits the " + who.name + " for " + damage + " points of damage.";
+        }
+        return "The " + this.name + " missed the " + who.name + " doing no damage.";
+
+    }
 };
 
 class FightingGame {
     constructor() {
         this.fighters = [
-            hero = new Fighter("hero"),      // 0
-            villian = new Fighter("villian") // 1
+            new Fighter("Hero"),   // 0
+            new Fighter("Villian") // 1
         ];
     }
     status() {
-        return this.name
+        return this.fighters.map(f => f.status()).join("\n-----")
+    }
+    fight(a, b) {
+        const output = [];
+        let victor = {};
+        output.push("Fight " + a.name + " vs " + b.name);
+        victor = new Fighter("No one");
+        // Todo fight while both are alive
+        while (a.health > 0 && b.health > 0) {
+            // a tries to hit b
+            output.push(a.attack(b));
+            // b tries to hit a
+            output.push(b.attack(a));
+        };
+        if (a.health > 0) {
+            victor = a;
+        }
+        if (b.health > 0) {
+            victor = b
+        }
+        output.push("Finish the " + victor.name + " Wins!!!");
+        return output.join("\n");
     }
 };
-fg = FightingGame();
+fg = new FightingGame();
+console.log(fg.status());
+console.log(fg.fight(fg.fighters[0], fg.fighters[1]));
 console.log(fg.status());
